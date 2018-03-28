2689d0e666ab6b18fc976fe343702c0142455f47
Jonathan Strong <jstrong@legis.io>
2018-03-28 13:10:10 -0400

    adds readme examples as lib docs
    fixes a few things so they now run as tests as well. this will provide
    some content for the crate's entry page on docs.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+//! This library provides differentiable operations and tensors. The current
+//! backend is rust-ndarray.
+//!
+//! # Examples
+//!
+//! Here we are computing partial derivatives of `z = 2x^2 + 3y + 1`.
+//!
+//! ```rust
+//! extern crate autograd as ag;
+//! extern crate ndarray;
+//! # fn main() {
+//!
+//! let ref x = ag::placeholder(&[]);
+//! let ref y = ag::placeholder(&[]);
+//! let ref z = 2*x*x + 3*y + 1;
+//!
+//! // dz/dy
+//! let ref gy = ag::grad(&[z], &[y])[0];
+//!
+//! // dz/dx
+//! let ref gx = ag::grad(&[z], &[x])[0];
+//!
+//! // ddz/dx (differentiates `z` again)
+//! let ref ggx = ag::grad(&[gx], &[x])[0];
+//!
+//! // evaluation of symbolic gradients
+//! println!("{:?}", gy.eval(&[]));   // => Ok(3.)
+//! println!("{:?}", ggx.eval(&[]));  // => Ok(4.)
+//!
+//! // dz/dx requires to fill the placeholder `x`
+//! println!("{:?}", gx.eval(&[(x, &ndarray::arr0(2.).into_dyn())]));  // => Ok(8.)
+//! # }
+//! ```
+//!
+//! Another example: multi layer perceptron for MNIST digits classification.
+//!
+//! ```rust
+//! extern crate autograd as ag;
+//! use ag::gradient_descent_ops::Optimizer;
+//! # fn main() {
+//!
+//! // -- graph def --
+//! let ref x = ag::placeholder(&[-1, 28*28]);
+//! let ref y = ag::placeholder(&[-1]);
+//! let ref w = ag::variable(ag::ndarray_ext::glorot_uniform(&[28*28, 10]));
+//! let ref b = ag::variable(ag::ndarray_ext::zeros(&[1, 10]));
+//! let ref z = ag::matmul(x, w) + b;
+//! let ref loss = ag::reduce_mean(&ag::sparse_softmax_cross_entropy(z, y), &[0, 1], false);
+//! let ref params = [w, b];
+//! let ref grads = ag::grad(&[loss], params);
+//! let ref predictions = ag::argmax(z, -1, true);
+//! let ref accuracy = ag::reduce_mean(&ag::equal(predictions, y), &[0], false);
+//! let mut adam = ag::gradient_descent_ops::Adam::default();
+//! let ref update_ops = adam.compute_updates(params, grads);
+//!
+//! // -- dataset --
+//! // let ((x_train, y_train), (x_test, y_test)) = dataset::load();
+//! //
+//! // -- training loop --
+//! // for epoch in 0..30 {
+//!     // ...
+//!     // ag::run(update_ops, &[(x, &x_batch), (y, &y_batch)]);
+//! // }
+//! # }
+//! ```
 #[macro_use(s)]
 extern crate ndarray;
 extern crate rand;
